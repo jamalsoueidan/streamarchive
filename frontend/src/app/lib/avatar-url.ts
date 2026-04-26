6b20c2162fa9b9f4f6433c57ac49f6f378b4dbf4
Jamal Soueidan <jamal@soueidan.com>
2026-04-26 18:43:05 +0300

    make images go through cloudflare

--- frontend/src/app/lib/avatar-url.ts
+++ frontend/src/app/lib/avatar-url.ts
@@ -1,6 +1,13 @@
 export function generateAvatarUrl(url?: string, fullUrl: boolean = false) {
   const filename = url?.split("/").pop();
-  return (
-    (fullUrl ? process.env.NEXT_PUBLIC_BASE_URL : "") + `/avatar/${filename}`
-  );
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  if (baseUrl && filename) {
+    const hasThumb =
+      filename.endsWith(".png") ||
+      filename.endsWith(".jpg") ||
+      filename.endsWith(".jpeg");
+    const host = new URL(baseUrl).hostname.replace(/^www\./, "");
+    return `https://image.${host}/${hasThumb ? "thumbnail_" : ""}${filename}`;
+  }
+  return (fullUrl ? baseUrl : "") + `/avatar/${filename}`;
 }
